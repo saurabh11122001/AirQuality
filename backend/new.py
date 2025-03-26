68e38ca8169a23f246ea22d04a6e65fbd345bb10
saurabh <saurabhrautela2001@gmail.com>
2025-03-26 13:14:23 +0530

    added new file

--- backend/new.py
+++
@@ -1,8 +0,0 @@
-import os
-
-model_path = r"C:\Users\Saurabh\Desktop\AirQuality\backend\best_cnn_model.keras"
-
-if os.path.exists(model_path):
-    print("✅ Model file exists at:", model_path)
-else:
-    print("❌ Model file NOT found! Please check the path.")

9beda82edcc5fb9947cbe8ffe1c64e8cb7055bfe
saurabh <saurabhrautela2001@gmail.com>
2025-04-03 14:05:42 +0530

    changes

--- backend/app.py
+++ backend/app.py
@@ -176,10 +176,10 @@ def handle_exception(e):
     print(f"Unhandled exception: {e}")
     return jsonify({"error": "Something went wrong"}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
 # if __name__ == '__main__':
-#     port = int(os.environ.get("PORT", 5000))  # Default port 5000
-#     app.run(host="0.0.0.0", port=port, debug=True)
+#     app.run(debug=True)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Default port 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
